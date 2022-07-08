63b57cf1b4376dfa58230beac25577f4265d9cb5
a10230m <a10230m@gmail.com>
2022-07-08 03:45:26 +0000

    admin注文履歴・注文詳細スキップ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -19,4 +19,13 @@ class Admin::CustomersController < ApplicationController
     redirect_to admin_customer_path(@customer.id)
   end
 
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email, :password, :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_active)
+
+  end
+
 end
